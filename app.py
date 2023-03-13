64ea95158cbbfd615eb75caa896d2fc8a46094c9
eshabaweja <bawejaesha02@gmail.com>
2023-03-14 00:37:00 +0530

    ✨  feat: display uploaded image in result

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,4 +9,13 @@ def home():
 
 @app.route("/results",methods=["POST"])
 def results():
-    return render_template("results.html")
+    if request.method=="POST":
+        # getting uploaded file
+        uploaded_img = request.files["uploaded_img"]
+        img_filename = uploaded_img.filename
+        # saving it to static/uploads/
+        filename = uploaded_img.filename
+        file_path = os.path.join(app.root_path, 'static', 'uploads', filename)
+        uploaded_img.save(file_path)
+
+    return render_template("results.html", img_filename=img_filename)
