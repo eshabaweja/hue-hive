ef32ca3e3e772b10ee2ccfaf2ba587b509dd4019
eshabaweja <bawejaesha02@gmail.com>
2023-03-13 08:39:02 +0530

    ✨  feat: upload images to form

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/results",methods=["POST"])
+def results():
+    return render_template("results.html")
